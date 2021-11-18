2fdc7710c9cb62796b66ed060d1a09fb9c130980
Vitaly Saprygin <82073769+Vsaprygin@users.noreply.github.com>
2021-11-18 17:26:19 +0700

    Add files via upload

---
+++ json.js
@@ -0,0 +1,33 @@
+// эта функция сработает при нажатии на кнопку
+function sendJSON() {
+  // считываем объекты
+  let text = document.querySelector('#new_text');
+ 
+  // формируем данные
+  let data = [{
+    Datatime: text.value || 'ничего не написали',
+  }];
+ 
+  // отправка и анализ
+  let url = "https://json.vsaprygin.ru/json/json.php";
+  fetch(url, 
+    {
+      method:'POST', 
+      headers: {'Content-Type': 'application/json;charset=utf-8'}, 
+      body: JSON.stringify(data),
+    }
+  )
+  .then((resp) => {
+    if (resp.ok) {
+      // очищаем поле
+      new_text.value = "";
+    } else {
+      result.innerHTML = 'ERROR: ' + resp.status + ' ' + resp.statusText;
+    }
+    return resp.text();
+  })
+  .then((data) => {
+    result.innerHTML += data;
+  })
+
+}
